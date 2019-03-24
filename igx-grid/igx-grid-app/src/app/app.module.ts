6229767d79cf66415d71a708333b93bf4233e536
ymita <ymita0617@gmail.com>
2019-03-24 10:19:33 +0900

    add BrowserAnimationsModule

--- igx-grid/igx-grid-app/src/app/app.module.ts
+++ igx-grid/igx-grid-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // 追加
 import { IgxGridModule } from 'igniteui-angular'; // 追加
 import 'hammerjs'; // 追加
 
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule, // 追加
     IgxGridModule // 追加
   ],
   providers: [],
